6092a25a68f5f55dbcf9156ecb8b7d421d0f50c7
Mohamed <mmuseaden@gmail.com>
2022-03-24 00:45:48 +0000

    first commit

---
+++ project-1/script.py
@@ -0,0 +1,17 @@
+import numpy as np
+import requests
+
+x = np.arange(15, dtype=np.int64).reshape(3, 5)
+x[1:, ::2] = -99
+print(x)
+
+
+print(x.max(axis=1))
+
+rng = np.random.default_rng()
+samples = rng.normal(size=2500)
+print(samples)
+
+ali = requests.get('https://puppy-ali-scrapper.herokuapp.com/aliscraper/1005003352859996')
+
+print(ali.text)
